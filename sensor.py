6c9b4c9214d143070f7d29a752dfbc9593e4eaba
Matthew Yen <mgyen@shaw.ca>
2020-02-28 09:04:54 -0800

    added test sensor code

---
+++ sensor.py
@@ -0,0 +1,22 @@
+from RPi import GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+left_sensor = 11
+right_sensor = 9
+
+GPIO.setup(left_sensor, GPIO.IN)
+GPIO.setup(right_sensor, GPIO.IN)
+
+try:
+	while True:
+		if not GPIO.input(left_sensor):
+			print("Robot is straying off to the right, move left captain!")
+		elif not GPIO.input(right_sensor):
+			print("Robot is straying off to the left, move right captain!")
+		else:
+			print("Following the line!")
+		sleep(0.2)
+except:
+	GPIO.cleanup()
