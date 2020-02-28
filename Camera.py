3fdcd7cbd727c3257274c95c07221e560e59a963
mpchow <mattchow918@gmail.com>
2020-02-28 09:22:52 -0800

    Camera test

--- Camera.py
+++ Camera.py
@@ -1,7 +1,7 @@
 from picamera import PiCamera
 from time import sleep
-from adafruit_st7735r import ST7735R
-import adafruit_imageload
+# from adafruit_st7735r import ST7735R
+# import adafruit_imageload
 
 camera = PiCamera()
 
@@ -9,22 +9,22 @@ camera = PiCamera()
 # unable to see if using remote access like SSH or VNC
 camera.start_preview()
 sleep(5)                                                # must sleep for at least 2 seconds so can sense light levels
-camera.capture('image.bmp', resize=(128, 128))
-displayImg()
+camera.capture('/home/pi/Desktop/image.bmp', resize=(128, 128))
+# displayImg()
 camera.stop_preview()
 
 # make preview slightly see through so we can see errors
-camera.start_preview(alpha=200)
+# camera.start_preview(alpha=200)
 
 # Display image on the LCD
-def displayImg(fileName):
-    # Loads the bitmap image
-    bitmap, palette = adafruit_imageload.load(fileName, bitmap=displayio.Bitmap, palette=displayio.Palette)
-    # Create a TileGrid to store the bitmap
-    tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
-    # Create a Group to store the TileGrid
-    group = displayio.Group()
-    # Add the titleGrid to the Group
-    group.append(tile_grid)
-    # Show the group
-    display.show(group)
+# def displayImg(fileName):
+#     # Loads the bitmap image
+#     bitmap, palette = adafruit_imageload.load(fileName, bitmap=displayio.Bitmap, palette=displayio.Palette)
+#     # Create a TileGrid to store the bitmap
+#     tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
+#     # Create a Group to store the TileGrid
+#     group = displayio.Group()
+#     # Add the titleGrid to the Group
+#     group.append(tile_grid)
+#     # Show the group
+#     display.show(group)
