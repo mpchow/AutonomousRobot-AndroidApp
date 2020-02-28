abc737190677dafe548f7f6af39c6dfbc2115910
briellelaw <lawbrielle@gmail.com>
2020-02-28 10:45:10 -0800

    attempt at client side of continuous live stream

--- Camera.py
+++ Camera.py
@@ -1,9 +1,16 @@
 from picamera import PiCamera
 from time import sleep
+import io
+import socket
+import struct
+import time
+import picamera
 # from adafruit_st7735r import ST7735R
 # import adafruit_imageload
 
 camera = PiCamera()
+
+stream = picamera.PiCameraCircularIO(camera, splitter_port=2)
 
 # opens camera preview 
 # unable to see if using remote access like SSH or VNC
@@ -14,12 +21,6 @@ camera.capture('/home/pi/Desktop/image.bmp', resize=(128, 128))
 camera.stop_preview()
 
 def takePicture(imageName):
-    
-
-
-
-
-
 
 
 # make preview slightly see through so we can see errors
@@ -44,3 +45,56 @@ def record():
     sleep(5)
     camera.stop_recording()
     camera.stop_preview()
+
+def stream():
+    camera.start_recording(stream, format='h264', splitter_port=2)
+    camera.wait_recording(10, splitter_port=2)
+    camera.stop_recording(splitter_port=2)
+
+def clearStream():
+    clear()
+
+
+# Reference for sending stream: https://picamera.readthedocs.io/en/release-1.10/recipes1.html
+# Connect a client socket to my_server:8000 (change my_server to the
+# hostname of your server)
+client_socket = socket.socket()
+client_socket.connect(('my_server', 8000))
+
+# Make a file-like object out of the connection
+connection = client_socket.makefile('wb')
+try:
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)
+        # Start a preview and let the camera warm up for 2 seconds
+        camera.start_preview()
+        time.sleep(2)
+
+        # Note the start time and construct a stream to hold image data
+        # temporarily (we could write it directly to connection but in this
+        # case we want to find out the size of each capture first to keep
+        # our protocol simple)
+        start = time.time()
+        stream = io.BytesIO()
+        for foo in camera.capture_continuous(stream, 'jpeg'):
+            # Write the length of the capture to the stream and flush to
+            # ensure it actually gets sent
+            connection.write(struct.pack('<L', stream.tell()))
+            connection.flush()
+            # Rewind the stream and send the image data over the wire
+            stream.seek(0)
+            connection.write(stream.read())
+            # If we've been capturing for more than 30 seconds, quit
+            if time.time() - start > 30:
+                break
+            # Reset the stream for the next capture
+            stream.seek(0)
+            stream.truncate()
+    # Write a length of zero to the stream to signal we're done
+    connection.write(struct.pack('<L', 0))
+finally:
+    connection.close()
+    client_socket.close()
+
+
+
