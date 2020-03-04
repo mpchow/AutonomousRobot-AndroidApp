e5d0e3b63eb2ef71f61622d2894f86a8b21fe762
Matthew Stefansson <stefansson30952@gmail.com>
2020-03-04 09:00:11 -0800

    Update LCD.py

--- LCD.py
+++ LCD.py
@@ -8,67 +8,110 @@ import adafruit_rgb_display.st7735 as st7735        # pylint: disable=unused-imp
 import adafruit_rgb_display.ssd1351 as ssd1351      # pylint: disable=unused-import
 import adafruit_rgb_display.ssd1331 as ssd1331      # pylint: disable=unused-import
 
-# First define some constants to allow easy resizing of shapes.
-BORDER = 5
-FONTSIZE = 24
+def writeText():
+    # First define some constants to allow easy resizing of shapes.
+    BORDER = 5
+    FONTSIZE = 24
+    # Configuration for CS and DC pins (these are PiTFT defaults):
+    cs_pin = digitalio.DigitalInOut(board.CE0)
+    dc_pin = digitalio.DigitalInOut(board.D25)
+    reset_pin = digitalio.DigitalInOut(board.D24)
 
-# Configuration for CS and DC pins (these are PiTFT defaults):
-cs_pin = digitalio.DigitalInOut(board.CE0)
-dc_pin = digitalio.DigitalInOut(board.D25)
-reset_pin = digitalio.DigitalInOut(board.D24)
+    # Config for display baudrate (default max is 24mhz):
+    BAUDRATE = 24000000
 
-# Config for display baudrate (default max is 24mhz):
-BAUDRATE = 24000000
+    # Setup SPI bus using hardware SPI:
+    spi = board.SPI()
+    disp = st7735.ST7735R(spi, rotation=270, height=128, x_offset=2, y_offset=3,   # 1.44" ST7735R
+                           cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE)
+    # pylint: enable=line-too-long
 
-# Setup SPI bus using hardware SPI:
-spi = board.SPI()
+    # Create blank image for drawing.
+    # Make sure to create image with mode 'RGB' for full color.
+    if disp.rotation % 180 == 90:
+        height = disp.width   # we swap height/width to rotate it to landscape!
+        width = disp.height
+    else:
+        width = disp.width   # we swap height/width to rotate it to landscape!
+        height = disp.height
 
-# pylint: disable=line-too-long
-# Create the display:
-#disp = st7789.ST7789(spi, rotation=90,                            # 2.0" ST7789
-#disp = st7789.ST7789(spi, height=240, y_offset=80, rotation=180,  # 1.3", 1.54" ST7789
-#disp = st7789.ST7789(spi, rotation=90, width=135, height=240, x_offset=53, y_offset=40, # 1.14" ST7789
-#disp = hx8357.HX8357(spi, rotation=180,                           # 3.5" HX8357
-#disp = st7735.ST7735R(spi, rotation=90,                           # 1.8" ST7735R
-disp = st7735.ST7735R(spi, rotation=270, height=128, x_offset=2, y_offset=3,   # 1.44" ST7735R
-#disp = st7735.ST7735R(spi, rotation=90, bgr=True,                 # 0.96" MiniTFT ST7735R
-#disp = ssd1351.SSD1351(spi, rotation=180,                         # 1.5" SSD1351
-#disp = ssd1351.SSD1351(spi, height=96, y_offset=32, rotation=180, # 1.27" SSD1351
-#disp = ssd1331.SSD1331(spi, rotation=180,                         # 0.96" SSD1331
-#disp = ili9341.ILI9341(spi, rotation=90,                           # 2.2", 2.4", 2.8", 3.2" ILI9341
-                       cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE)
-# pylint: enable=line-too-long
+    image = Image.new('RGB', (width, height))
 
-# Create blank image for drawing.
-# Make sure to create image with mode 'RGB' for full color.
-if disp.rotation % 180 == 90:
-    height = disp.width   # we swap height/width to rotate it to landscape!
-    width = disp.height
-else:
-    width = disp.width   # we swap height/width to rotate it to landscape!
-    height = disp.height
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
 
-image = Image.new('RGB', (width, height))
+    # Draw a green filled box as the background
+    draw.rectangle((0, 0, width, height), fill=(0, 255, 0))
+    disp.image(image)
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
+    # Draw a smaller inner purple rectangle
+    draw.rectangle((BORDER, BORDER, width - BORDER - 1, height - BORDER - 1),
+                   fill=(170, 0, 136))
 
-# Draw a green filled box as the background
-draw.rectangle((0, 0, width, height), fill=(0, 255, 0))
-disp.image(image)
+    # Load a TTF Font
+    font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', FONTSIZE)
 
-# Draw a smaller inner purple rectangle
-draw.rectangle((BORDER, BORDER, width - BORDER - 1, height - BORDER - 1),
-               fill=(170, 0, 136))
-
-# Load a TTF Font
-font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', FONTSIZE)
-
-# Draw Some Text
-text = "Hello World!"
-(font_width, font_height) = font.getsize(text)
-draw.text((width//2 - font_width//2, height//2 - font_height//2),
+    # Draw Some Text
+    text = "Hello World!"
+    (font_width, font_height) = font.getsize(text)
+    draw.text((width//2 - font_width//2, height//2 - font_height//2),
           text, font=font, fill=(255, 255, 0))
 
-# Display image.
-disp.image(image)
+    # Display image.
+    disp.image(image)
+
+def writeImages():
+    # First define some constants to allow easy resizing of shapes.
+    BORDER = 5
+    FONTSIZE = 24
+    # Configuration for CS and DC pins (these are PiTFT defaults):
+    cs_pin = digitalio.DigitalInOut(board.CE0)
+    dc_pin = digitalio.DigitalInOut(board.D25)
+    reset_pin = digitalio.DigitalInOut(board.D24)
+
+    # Config for display baudrate (default max is 24mhz):
+    BAUDRATE = 24000000
+
+    # Setup SPI bus using hardware SPI:
+    spi = board.SPI()
+    disp = st7735.ST7735R(spi, rotation=270, height=128, x_offset=2, y_offset=3,   # 1.44" ST7735R
+                           cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE)
+    # pylint: enable=line-too-long
+
+    # Create blank image for drawing.
+    # Make sure to create image with mode 'RGB' for full color.
+    if disp.rotation % 180 == 90:
+        height = disp.width   # we swap height/width to rotate it to landscape!
+        width = disp.height
+    else:
+        width = disp.width   # we swap height/width to rotate it to landscape!
+        height = disp.height
+    image = Image.new('RGB', (width, height))
+ 
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+ 
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    disp.image(image)
+    
+    image = Image.open("blinka.jpg")
+    
+    # Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+ 
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+ 
+    # Display image.
+    disp.image(image)
