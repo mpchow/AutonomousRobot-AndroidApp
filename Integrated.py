81c548e730c9b584b381b98245bb525ef9156c87
Andrea Shao <andrea25shao@gmail.com>
2020-03-06 10:27:09 -0800

    Changed to 3 sensors

--- Integrated.py
+++ Integrated.py
@@ -132,54 +132,38 @@ def controller():
 
 class Error:
     def __init__(self):
-        # sensorVal[0] = far left, sensorVal[1] = mid left, sensorVal[2] = middle
-        # sensorVal[3] = mid right, sensorVal[4] = far right
-        self.sensorVal = [0, 0, 0, 0, 0]
+        # sensorVal[0] = left, sensorVal[1] = middle, sensorVal[2] = right
+        self.sensorVal = [0, 0, 0]
         self.error = 0
         self.prevError = 0
         self.integral = 0
-        # Kp, Kd, Ki determined through experimentation
         self.Kp = 0.0005
         self.Kd = 0
         self.Ki = 0
 
-
     def calculateError(self):
         # 0th index is for left sensor, 4th is the rightmost sensor from a topdown view
         # Shift array elements to create one sum
-        errorTotal = (self.sensorVal[0] * 10000)
-        errorTotal += (self.sensorVal[1] * 1000)
-        errorTotal += (self.sensorVal[2] * 100)
-        errorTotal += (self.sensorVal[3] * 10)
-        errorTotal += self.sensorVal[4]
+        errorTotal = (self.sensorVal[0] * 100)
+        errorTotal += (self.sensorVal[1] * 10)
+        errorTotal += self.sensorVal[2] 
 
-        if (errorTotal == 1):           # far right sensor triggered
-            self.error = 4
-        elif (errorTotal == 11):        # far & mid right sensors triggered
-            self.error = 3
-        elif (errorTotal == 10):        # mid right sensor triggered
-            self.error = 2
-        elif (errorTotal == 110):       # middle & mid right sensors triggered
-            self.error = 1
-        elif (errorTotal == 100):       # middle sensor triggered
-            self.error = 0
-        elif (errorTotal == 1100):      # middle & mid left sensors triggered
-            self.error = -1
-        elif (errorTotal == 1000):      # mid left sensor triggered
-            self.error = -2
-        elif (errorTotal == 11000):     # far & mid left sensors triggered
-            self.error = -3
-        elif (errorTotal == 10000):     # far left sensor triggered
+        if (errorTotal == 1):           # right sensor triggered
             self.error = -4
+        elif (errorTotal == 11):        # middle and right sensors triggered
+            self.error = -1
+        elif (errorTotal == 10):        # middle sensor triggered
+            self.error = 0
+        elif (errorTotal == 110):       # middle and left sensors triggered
+            self.error = 1
+        elif (errorTotal == 100):       # left sensor triggered
+            self.error = 4
 
     def getOptics(self):
-        # Read sensor values and input them into sensorVal array
         sens1 = GPIO.input(sensor1)
         sens2 = GPIO.input(sensor2)
         sens3 = GPIO.input(sensor3)
-        sens4 = GPIO.input(sensor4)
-        sens5 = GPIO.input(sensor5)
-        self.sensorVal = [sens1, sens2, sens3, sens4, sens5]
+        self.sensorVal = [sens1, sens2, sens3]
 
     def calculatePID(self):
         # Calculates PID value based on new sensor inputs and past values (prevError, integral)
