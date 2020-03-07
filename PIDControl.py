fdbe0248c809006a8144d2023c1dc126fe5fc68b
mattyen <mgyen@shaw.ca>
2020-03-07 15:41:05 -0800

    PID code

--- PIDControl.py
+++ PIDControl.py
@@ -2,32 +2,26 @@ from RPi import GPIO
 from adafruit_motorkit import MotorKit
 import digitalio
 import board
-from PIL import Image, ImageDraw, ImageFont
 import time
 
 GPIO.setmode(GPIO.BCM)
 
 # Assign sensor pins
-sensor1 = 26
+sensor1 = 13
 sensor2 = 19
-sensor3 = 13
-sensor4 = 6
-sensor5 = 5
-
+sensor3 = 26
 # Setup GPIO inputs
 GPIO.setup(sensor1, GPIO.IN)
 GPIO.setup(sensor2, GPIO.IN)
 GPIO.setup(sensor3, GPIO.IN)
-GPIO.setup(sensor4, GPIO.IN)
-GPIO.setup(sensor5, GPIO.IN)
 
 
 def controller():
     #Instantiate the motorkit instance
     kit = MotorKit()
     #Initially start the motors at same speed so they are running straight
-    kit.motor1.throttle = 0.20
-    kit.motor2.throttle = 0.20
+    kit.motor1.throttle = 0.0
+    kit.motor2.throttle = 0.0
     #Instantiate the error class to calculate things for us
     error = Error()
     #Loop for the feedback loop
@@ -36,10 +30,15 @@ def controller():
             #Calculate the PID value
             error.getOptics()
             PID = error.calculatePID()
-            print(PID)
+            if (error.count == 20):
+                kit.motor1.throttle = 0.0
+                kit.motor2.throttle = 0.0
+                print("Stop")
+                break
+            time.sleep(0.05)
             #summ the pid value with the base throttle of 0.75 to turn left or right based on imbalances in the throttle values
-            kit.motor1.throttle = 0.20 + PID #Assuming this is the left motor
-            kit.motor2.throttle = 0.20 - PID #Assuming this is the right motor
+            kit.motor1.throttle = 0.32 + PID #Assuming this is the left motor
+            kit.motor2.throttle = 0.32 - PID #Assuming this is the right motor
 
     except KeyboardInterrupt:
         kit.motor1.throttle = 0.0
@@ -52,9 +51,10 @@ class Error:
         self.error = 0
         self.prevError = 0
         self.integral = 0
-        self.Kp = 0.05
-        self.Kd = 0
+        self.Kp = 0.055
+        self.Kd = 0.13
         self.Ki = 0
+        self.count = 0
 
 
     def calculateError(self):
@@ -65,15 +65,25 @@ class Error:
         errorTotal += self.sensorVal[2]
 
         if (errorTotal == 1):           # right sensor triggered
-            self.error = -4
+            self.count = 0
+            self.error = -2
         elif (errorTotal == 11):        # middle and right sensors triggered
+            self.count = 0
             self.error = -1
         elif (errorTotal == 10):        # middle sensor triggered
+            self.count = 0
             self.error = 0
         elif (errorTotal == 110):       # middle and left sensors triggered
+            self.count = 0
             self.error = 1
         elif (errorTotal == 100):       # left sensor triggered
-            self.error = 4
+            self.count = 0
+            self.error = 2
+        elif (errorTotal == 111):       # all sensors triggered, most likely crossover
+            self.count = 0
+            self.error = 0
+        elif (errorTotal == 0):
+            self.count = self.count + 1
 
 
     def getOptics(self):
