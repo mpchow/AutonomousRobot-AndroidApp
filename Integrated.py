72705188ba1d0351069c5a070cadb09b961aa653
Matthew Stefansson <stefansson30952@gmail.com>
2020-03-09 15:44:49 -0700

    Update Integrated.py

--- Integrated.py
+++ Integrated.py
@@ -95,7 +95,6 @@ def animation():
         writeImages("thirdGear.jpg")
         count = 0   # return to first image
 
-
 def parseJson(byteStream):
     # Decode UTF-8 bytes to unicode
     # To make valid JSON, replace single quotes with double quotes
@@ -104,16 +103,14 @@ def parseJson(byteStream):
     jsonList = json.load(jsonStream)
     parsedJson = json.dumps(jsonList, indent=4, sort_keys=True)
     print(parsedJson)
-        
-        
+
 def controller():
-    global count
     #Instantiate the motorkit instance
     kit = MotorKit()
     #Initially start the motors at same speed so they are running straight
-    kit.motor1.throttle = 0.25
-    kit.motor2.throttle = 0.25
-    #Instantiate the error class for calculations
+    kit.motor1.throttle = 0.0
+    kit.motor2.throttle = 0.0
+    #Instantiate the error class to calculate things for us
     error = Error()
     #Loop for the feedback loop
     try:
@@ -121,13 +118,15 @@ def controller():
             #Calculate the PID value
             error.getOptics()
             PID = error.calculatePID()
-
+            
             # If PID is less than threshold, robot turn right
             if (PID < -28):
+                # need to change the values so the right image is played
                 writeImages("rightArrow.jpg")
 
             # If PID greater than threshold, robot turn left
             elif(PID > 28):
+                # need to change the values so the right image is played
                 writeImages("leftArrow.jpg")
             
             # If robot going straight or performing negligble turns, play animation
@@ -135,9 +134,14 @@ def controller():
                 count += 1
                 animation()
 
+            if (error.count == 22):
+                kit.motor1.throttle = 0.0
+                kit.motor2.throttle = 0.0
+                break
+            time.sleep(0.05)
             #sum the pid value with the base throttle of 0.75 to turn left or right based on imbalances in the throttle values
-            kit.motor1.throttle = 0.25 + PID #Assuming this is the left motor
-            kit.motor2.throttle = 0.25 - PID #Assuming this is the right motor
+            kit.motor1.throttle = 0.30 + PID #Assuming this is the left motor
+            kit.motor2.throttle = 0.30 - PID #Assuming this is the right motor
 
     except KeyboardInterrupt:
         kit.motor1.throttle = 0.0
@@ -150,33 +154,47 @@ class Error:
         self.error = 0
         self.prevError = 0
         self.integral = 0
-        self.Kp = 0.0005
-        self.Kd = 0
+        self.Kp = 0.055
+        self.Kd = 0.13
         self.Ki = 0
+        self.count = 0
+
 
     def calculateError(self):
         # 0th index is for left sensor, 4th is the rightmost sensor from a topdown view
         # Shift array elements to create one sum
         errorTotal = (self.sensorVal[0] * 100)
         errorTotal += (self.sensorVal[1] * 10)
-        errorTotal += self.sensorVal[2] 
+        errorTotal += self.sensorVal[2]
 
         if (errorTotal == 1):           # right sensor triggered
-            self.error = -4
+            self.count = 0
+            self.error = -2
         elif (errorTotal == 11):        # middle and right sensors triggered
+            self.count = 0
             self.error = -1
         elif (errorTotal == 10):        # middle sensor triggered
+            self.count = 0
             self.error = 0
         elif (errorTotal == 110):       # middle and left sensors triggered
+            self.count = 0
             self.error = 1
         elif (errorTotal == 100):       # left sensor triggered
-            self.error = 4
+            self.count = 0
+            self.error = 2
+        elif (errorTotal == 111):       # all sensors triggered, most likely crossover
+            self.count = 0
+            self.error = 0
+        elif (errorTotal == 0):
+            self.count = self.count + 1
+
 
     def getOptics(self):
         sens1 = GPIO.input(sensor1)
         sens2 = GPIO.input(sensor2)
         sens3 = GPIO.input(sensor3)
         self.sensorVal = [sens1, sens2, sens3]
+
 
     def calculatePID(self):
         # Calculates PID value based on new sensor inputs and past values (prevError, integral)
