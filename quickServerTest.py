a02dd6589a97fff2de37da3f66e2daabf30ff801
mpchow <mattchow918@gmail.com>
2020-03-09 20:55:18 -0700

    Tested socket connection with pi

--- quickServerTest.py
+++ quickServerTest.py
@@ -4,7 +4,9 @@ PORT = 5000       # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((socket.gethostname(), PORT))
+    print(socket.gethostname())
     s.listen()
-    conn, addr = s.accept()
+    (conn, addr) = s.accept()
+    print("Connected")
     with conn:
         print('Connected by', addr)
