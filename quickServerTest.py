0e2d713a821d323bdc804c0ff22275ac520f6ff7
mpchow <mattchow918@gmail.com>
2020-03-11 09:10:56 -0700

    Testing json

--- quickServerTest.py
+++ quickServerTest.py
@@ -3,6 +3,14 @@ import socket
 PORT = 5008       # Port to listen on (non-privileged ports are > 1023)
 HOST = ''
 
+def parseJson(byteStream):
+    # Decode UTF-8 bytes to unicode
+    # To make valid JSON, replace single quotes with double quotes
+    jsonStream = byteStream.decode('utf8').replace("'", '"')
+    # Load JSON to Python list
+    jsonList = json.load(jsonStream)
+    parsedJson = json.dumps(jsonList, indent=4, sort_keys=True)
+    print(parsedJson)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -17,5 +25,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         input = conn.recv(1024)
         print(input)
         input = conn.recv(1024)
-        print(input)
+        parseJson(input)
         s.close()
