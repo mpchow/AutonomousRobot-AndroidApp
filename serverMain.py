2f44620e7b8813da1f6071c8b8348bf74bf84fee
mattyen <mgyen@shaw.ca>
2020-03-12 21:15:34 -0700

    Changing autonomous

--- serverMain.py
+++ serverMain.py
@@ -14,7 +14,7 @@ import adafruit_rgb_display.ssd1331 as ssd1331      # pylint: disable=unused-imp
 import json
 import socket
 
-PORT = 5022      # Port to listen on (non-privileged ports are > 1023)
+PORT = 5023      # Port to listen on (non-privileged ports are > 1023)
 HOST = ''
 
 GPIO.setmode(GPIO.BCM)
@@ -41,6 +41,67 @@ BAUDRATE = 24000000
 spi = board.SPI()
 # 1.44" ST7735R
 disp = st7735.ST7735R(spi, rotation=270, height=128, x_offset=2, y_offset=3, cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE)
+
+class Error:
+    def __init__(self):
+        # sensorVal[0] = left, sensorVal[1] = middle, sensorVal[2] = right
+        self.sensorVal = [0, 0, 0]
+        self.error = 0
+        self.prevError = 0
+        self.integral = 0
+        self.Kp = 0.08
+        self.Kd = 0.13
+        self.Ki = 0
+        # count for stopping motors
+        self.count = 0
+
+
+    def calculateError(self):
+        # 0th index is for left sensor, 4th is the rightmost sensor from a topdown view
+        # Shift array elements to create one sum
+        errorTotal = (self.sensorVal[0] * 100)
+        errorTotal += (self.sensorVal[1] * 10)
+        errorTotal += self.sensorVal[2]
+
+        if (errorTotal == 1):           # right sensor triggered
+            self.count = 0
+            self.error = -1.5
+        elif (errorTotal == 11):        # middle and right sensors triggered
+            self.count = 0
+            self.error = -1
+        elif (errorTotal == 10):        # middle sensor triggered
+            self.count = 0
+            self.error = 0
+        elif (errorTotal == 110):       # middle and left sensors triggered
+            self.count = 0
+            self.error = 1
+        elif (errorTotal == 100):       # left sensor triggered
+            self.count = 0
+            self.error = 1.5
+        elif (errorTotal == 111):       # all sensors triggered, most likely crossover
+            self.count = 0
+            self.error = 0
+        elif (errorTotal == 0):
+            self.count = self.count + 1
+
+
+    def getOptics(self):
+        sens1 = GPIO.input(sensor1)
+        sens2 = GPIO.input(sensor2)
+        sens3 = GPIO.input(sensor3)
+        self.sensorVal = [sens1, sens2, sens3]
+
+
+    def calculatePID(self):
+        # Calculates PID value based on new sensor inputs and past values (prevError, integral)
+        self.calculateError()           # adjust error values based on sensor readings
+        self.integral += self.error
+        pidValue = self.Kp * self.error + self.Kd * (self.error - self.prevError) + self.Ki * self.integral
+        self.prevError = self.error     # set prevError to new error for next iteration
+        return pidValue
+
+#Instantiate the error class to calculate things for us
+error = Error()
 
 # Function to parse json
 def parseJson(byteStream):
@@ -114,12 +175,8 @@ def writeImages(imageName):
     disp.image(image)
 
 def controller(kit):
+    global error
 
-    #Initially start the motors at same speed so they are running straight
-    kit.motor1.throttle = 0.0
-    kit.motor2.throttle = 0.0
-    #Instantiate the error class to calculate things for us
-    error = Error()
     #Loop for the feedback loop
     try:
         #Calculate the PID value
@@ -143,64 +200,6 @@ def controller(kit):
     except KeyboardInterrupt:
         kit.motor1.throttle = 0.0
         kit.motor2.throttle = 0.0
-
-class Error:
-    def __init__(self):
-        # sensorVal[0] = left, sensorVal[1] = middle, sensorVal[2] = right
-        self.sensorVal = [0, 0, 0]
-        self.error = 0
-        self.prevError = 0
-        self.integral = 0
-        self.Kp = 0.08
-        self.Kd = 0.13
-        self.Ki = 0
-        # count for stopping motors
-        self.count = 0
-
-
-    def calculateError(self):
-        # 0th index is for left sensor, 4th is the rightmost sensor from a topdown view
-        # Shift array elements to create one sum
-        errorTotal = (self.sensorVal[0] * 100)
-        errorTotal += (self.sensorVal[1] * 10)
-        errorTotal += self.sensorVal[2]
-
-        if (errorTotal == 1):           # right sensor triggered
-            self.count = 0
-            self.error = -1.5
-        elif (errorTotal == 11):        # middle and right sensors triggered
-            self.count = 0
-            self.error = -1
-        elif (errorTotal == 10):        # middle sensor triggered
-            self.count = 0
-            self.error = 0
-        elif (errorTotal == 110):       # middle and left sensors triggered
-            self.count = 0
-            self.error = 1
-        elif (errorTotal == 100):       # left sensor triggered
-            self.count = 0
-            self.error = 1.5
-        elif (errorTotal == 111):       # all sensors triggered, most likely crossover
-            self.count = 0
-            self.error = 0
-        elif (errorTotal == 0):
-            self.count = self.count + 1
-
-
-    def getOptics(self):
-        sens1 = GPIO.input(sensor1)
-        sens2 = GPIO.input(sensor2)
-        sens3 = GPIO.input(sensor3)
-        self.sensorVal = [sens1, sens2, sens3]
-
-
-    def calculatePID(self):
-        # Calculates PID value based on new sensor inputs and past values (prevError, integral)
-        self.calculateError()           # adjust error values based on sensor readings
-        self.integral += self.error
-        pidValue = self.Kp * self.error + self.Kd * (self.error - self.prevError) + self.Ki * self.integral
-        self.prevError = self.error     # set prevError to new error for next iteration
-        return pidValue
 
 
 #Instantiate the motorkit instance
@@ -236,7 +235,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
             if (mode == 'Autonomous'):
                 controller(kit)
-            else:
+            elif (mode == 'Remote Control'):
                 Type = jsonObj.get("Type")
                 if (Type == 'Forward'):
                     straight(kit)
@@ -246,6 +245,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     turnRight(kit)
                 else:
                     off(kit)
-
+            else:
+                off(kit)
 
         s.close()
